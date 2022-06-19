c76c9294bcd0da56672fe26395509300ad30178c
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-06-19 16:13:43 +0530

    usage of redux method, bindActionCreators

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
@@ -48,11 +49,18 @@ const unsubscribe = store.subscribe(() => {
   console.log("Update state = ", store.getState());
 });
 
-store.dispatch(orderCake(1));
-store.dispatch(orderCake(2));
-store.dispatch(orderCake(1));
+// store.dispatch(orderCake(1));
+// store.dispatch(orderCake(2));
+// store.dispatch(orderCake(1));
 
-store.dispatch(restockCake(4))
+// store.dispatch(restockCake(4))
+
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
+actions.orderCake(1)
+actions.orderCake(2);
+actions.orderCake(1);
+
+actions.restockCake(4)
 
 
 unsubscribe();
