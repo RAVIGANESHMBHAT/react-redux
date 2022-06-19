96518213124c5e244b00907322fbbe4f1d804bb3
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-06-19 17:50:52 +0530

    Redux for ordering cake and also Ice Creams with common reducer

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const redux = require("redux");
 const createStore = redux.createStore;
-const bindActionCreators = redux.bindActionCreators
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
-const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAT_RESTOCKED";
 
 //action creater
 function orderCake(qty = 1) {
@@ -14,14 +16,29 @@ function orderCake(qty = 1) {
 }
 
 function restockCake(qty = 1) {
-    return {
-        type: CAKE_RESTOCKED,
-        payload: qty
-    }
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
 }
 
 const initialState = {
   numOfCakes: 10,
+  numOfIceCreams: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -30,12 +47,22 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numOfCakes: state.numOfCakes - action.payload,
-          };
-      case CAKE_RESTOCKED:
-          return {
-              ...state,
-              numOfCakes: state.numOfCakes + action.payload
-          }
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
     default:
       return state;
   }
@@ -43,7 +70,7 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer);
 
-console.log("Initil state = ", store.getState())
+console.log("Initil state = ", store.getState());
 
 const unsubscribe = store.subscribe(() => {
   console.log("Update state = ", store.getState());
@@ -55,12 +82,15 @@ const unsubscribe = store.subscribe(() => {
 
 // store.dispatch(restockCake(4))
 
-const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
-actions.orderCake(1)
+const actions = bindActionCreators({ orderCake, restockCake, orderIceCream, restockIceCream }, store.dispatch);
+actions.orderCake(1);
 actions.orderCake(2);
 actions.orderCake(1);
 
-actions.restockCake(4)
+actions.restockCake(4);
 
+actions.orderIceCream(2)
+actions.orderIceCream(1);
+actions.restockIceCream(3)
 
 unsubscribe();
