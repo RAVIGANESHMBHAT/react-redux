deed0c4bd6ca37f9d4522eb7cd3f879979867de7
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-06-20 09:25:45 +0530

    redux-toolkit iceCream  example

--- react-redux-redux-toolkit-demo/index.js
+++ react-redux-redux-toolkit-demo/index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 
 console.log('Initial state = ', store.getState())
 const unsubscribe = store.subscribe(() => {
@@ -12,5 +13,9 @@ store.dispatch(cakeActions.ordered(1));
 
 store.dispatch(cakeActions.restocked(3));
 store.dispatch(cakeActions.restocked(1));
+
+store.dispatch(icecreamActions.ordered(1))
+store.dispatch(icecreamActions.ordered(2));
+store.dispatch(icecreamActions.restocked(3))
 
 unsubscribe()
